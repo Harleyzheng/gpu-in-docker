15a37a8e3f3c834f01a6495740e86c8255eb548e
Harleyzheng <zhengharley@gmail.com>
2022-12-22 17:33:51 +0800

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 app.get("/gpu", async (req, res) => {
   try {
     const browser = await puppeteer.launch({
